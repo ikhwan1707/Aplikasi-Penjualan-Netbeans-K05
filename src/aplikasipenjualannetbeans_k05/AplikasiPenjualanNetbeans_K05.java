ee9bf2926e14a14de212491ceb70adce6ff6b996
bayu prayoga <prayogabayu062@gmail.com>
2023-10-30 08:53:13 +0700

    Create Update-ipin

---
+++ src/aplikasipenjualannetbeans_k05/AplikasiPenjualanNetbeans_K05.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasipenjualannetbeans_k05;
+
+/**
+ *
+ * @author RnD02
+ */
+public class AplikasiPenjualanNetbeans_K05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
