7c9b97b92f4e4d18479d47bd0706d298022f6395
ArifineGT <Arifinkhun0811@gmail.com>
2023-11-06 08:41:38 +0700

    Update form koneksi -Ipin

--- src/koneksi.java
+++ src/koneksi.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
  *
  * @author SMKI Utama4
  */
-public class Koneksi {
+public class koneksi {
     
     public static Connection koneksi;
     //public static void main (String[] args) {
